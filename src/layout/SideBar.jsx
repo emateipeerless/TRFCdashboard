c3ba010aefc2d0f5d028d64f481987fd65f7d816
Ethan Matei <ematei@peerlesspump.com>
2026-01-08 09:58:13 -0500

    Site Style Overhual

--- src/layout/SideBar.jsx
+++ src/layout/SideBar.jsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSites } from '../SitesContext';
-import logo from '../assets/logo.png'
+import logo from '../assets/logoD.png'
 
 export default function Sidebar() {
   const { sites, loading } = useSites();
